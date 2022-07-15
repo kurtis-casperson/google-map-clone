3aa03edf9013000637f5aca59f69dedf5acc2e80
Kurtis <kurtis@edge.app>
2022-07-14 17:11:02 -0700

    Working map
    issue with setupMap function.  Causes error with navigation controls

---
+++ script.js
@@ -0,0 +1,46 @@
+// public token from mapbox
+const MAP_API_TOKEN =
+  'pk.eyJ1Ijoia3VydGpvaG4iLCJhIjoiY2w1ajV0azB6MDgwbDNkcXB4d204czJ0ZyJ9.ixlp543beQr3tkuwpzVqtQ'
+
+// var map = new mapboxgl.Map({
+//   accessToken: MAP_API_TOKEN,
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11',
+//   center: [-74.5, 40],
+//   zoom: 9,
+// })
+
+// Use geoLocation API to zoom in on the map to wherever you are located
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+})
+
+mapboxgl.accessToken = MAP_API_TOKEN
+// const map = new mapboxgl.Map({
+//   container: 'map', // container ID
+//   style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//   center: centerPostion, // starting position
+//   zoom: 9, // starting zoom
+// })
+
+// Add zoom and rotation controls to the map.
+
+// function setupMap(centerPostion) {
+// const map = new mapboxgl.Map({
+//   container: 'map', // container ID
+//   style: 'mapbox://styles/mapbox/streets-v11', // style URL
+//   center: centerPostion, // starting position
+//   zoom: 9, // starting zoom
+// })
+// }
+
+// default location was set up in chrome dev tools using the 'sensors' tool
+// function successLocation(position) {
+//   setupMap([position.coords.longitude, position.coords.latitude])
+//   console.log(position)
+// }
+
+// function errorLocation() {
+//   console.log('errorLocation')
+// }
