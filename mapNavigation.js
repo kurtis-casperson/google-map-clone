951f584be11c966807396e04dc7f112a9abd1235
Kurtis <kurtis@edge.app>
2022-07-20 16:12:10 -0700

    Add directions

--- mapNavigation.js
+++ mapNavigation.js
@@ -44,3 +44,33 @@ mapboxgl.accessToken = MAP_API_TOKEN
 // function errorLocation() {
 //   console.log('errorLocation')
 // }
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
+})
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAP_API_TOKEN,
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: centerPosition,
+    zoom: 15,
+  })
+
+  const navigationControls = new mapboxgl.NavigationControl()
+  map.addControl(navigationControls)
+
+  const directionControls = new MapboxDirections({
+    accessToken: MAP_API_TOKEN,
+  })
+  map.addControl(directionControls, 'top-left')
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
